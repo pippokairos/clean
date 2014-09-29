b1af00e9270a360d97bf9610405a6618b5a4e19b
kairos <fragazzo@shado.tv>
2014-09-29 17:37:12 +0200

    Multiple columns and json files

--- clean.rb
+++ clean.rb
@@ -2,42 +2,76 @@
 
 require "yaml"
 require "pg"
+require "fileutils"
+require "json"
+require "zlib"
+require "archive/tar/minitar"
+include Archive::Tar
+
+def is_int?(str) # method to define if a string represents an integer
+  !!(str =~ /\A[-+]?[0-9]+\z/)
+end
+
+def is_valid_json?(str) # method to define if a string is a valid json
+  JSON.parse(str)
+    return true
+  rescue JSON::ParserError
+    return false
+end
+
+def add_quotes(str)
+  "\"#{str}\""
+end
 
 def main
   config = YAML.load_file("config.yml")
-  
-  database = config['database']
-  field = config['field']
-  table = config['table']
-  json_file_name = config['json_file_name']
-  stock_file_name = config['stock_file_name']
-  json_file_name = "#{config['database']}_#{config['table']}_#{config['field']}.js"
+
+  database = config["database"]
+  table = config["table"]
+  includes = config["include"]
 
   conn = PG.connect(dbname: "#{database}")
-  conn.exec("BEGIN")
-  rows = conn.exec("SELECT #{field} FROM #{table}")
-  
-  File.open(json_file_name, "a") { |out| 
-    out.truncate(0) # delete file content 
-    out.print("{\"#{field}\":")
-    rows.each do |row|
-      out.print("#{row.values[0]},\n")
-    end
-    out.print("}")
-  } 
-  
-  puts "#{rows.count} rows written to #{json_file_name}."
-  
+  conn.exec("BEGIN") # for testing
+  includes.each do |field|
+    rows = conn.exec("SELECT #{field} FROM #{table}")
+    
+    FileUtils.mkdir_p "files" # mkdir if not existing
+    json_file_name = "files/#{database}_#{table}_#{field}.json"
+    
+    File.open(json_file_name, "a") { |out| 
+      out.truncate(0) # delete file content 
+      out.print("{\"#{field}\":[\n")
+      rows.each_with_index do |row, index|
+        value = row.values[0]
+        if index == rows.count - 1 # last row
+          # puts "#{field} --> is_int? #{is_int?(value)}; is_valid_json? #{is_valid_json?(value)}"
+          (is_int?(value) || is_valid_json?(value)) ? out.print("#{value}\n") : out.print("#{add_quotes(value)}\n")
+        else
+          (is_int?(value) || is_valid_json?(value)) ? out.print("#{value},\n") : out.print("#{add_quotes(value)},\n")
+        end
+      end
+      out.print("]}")
+    } 
+
+    puts "#{rows.count} rows written to #{json_file_name}"
+  end
+
   table_vals = conn.exec("SELECT * FROM #{table}")
+  stock_file_name = "files/#{config["stock_file_name"]}"
   File.open(stock_file_name, "a") { |out|
     out.print("#{table_vals.values[0]}")
   }
-  puts "#{rows.cmd_tuples()} rows copied to #{stock_file_name}"
   
+  tgz = Zlib::GzipWriter.new(File.open("stock.tgz", "wb"))
+  Minitar.pack(stock_file_name, tgz)
+  File.delete(stock_file_name)
+  
+  puts "#{table_vals.cmd_tuples()} rows stocked in tar archive"
+
   conn.exec("DELETE FROM #{table}")
-  puts "#{rows.cmd_tuples()} rows deleted from the table #{table}"
-    
-  conn.exec("ROLLBACK")
+  puts "#{table_vals.cmd_tuples()} rows deleted from the table #{table}"
+
+  conn.exec("ROLLBACK") # for testing
 end
 
 main()
