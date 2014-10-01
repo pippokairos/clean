f39e60984f43972d2d7518c20b00740a627ef021
kairos <fragazzo@shado.tv>
2014-10-01 09:42:50 +0200

    Formatted json files, code cleaned

--- clean.rb
+++ clean.rb
@@ -31,46 +31,54 @@ def main
   includes = config["include"]
 
   conn = PG.connect(dbname: "#{database}")
-  conn.exec("BEGIN") # for testing
+
   includes.each do |field|
     rows = conn.exec("SELECT #{field} FROM #{table}")
-    
+
     FileUtils.mkdir_p "files" # mkdir if not existing
     json_file_name = "files/#{database}_#{table}_#{field}.json"
-    
+
     File.open(json_file_name, "a") { |out| 
       out.truncate(0) # delete file content 
-      out.print("{\"#{field}\":[\n")
+      out.print("{\"#{field}\":\n  [\n")
+
       rows.each_with_index do |row, index|
         value = row.values[0]
         if index == rows.count - 1 # last row
-          # puts "#{field} --> is_int? #{is_int?(value)}; is_valid_json? #{is_valid_json?(value)}"
-          (is_int?(value) || is_valid_json?(value)) ? out.print("#{value}\n") : out.print("#{add_quotes(value)}\n")
+          (is_int?(value) || is_valid_json?(value)) ? out.print("    #{value}\n") : out.print("    #{add_quotes(value)}\n")
         else
-          (is_int?(value) || is_valid_json?(value)) ? out.print("#{value},\n") : out.print("#{add_quotes(value)},\n")
+          (is_int?(value) || is_valid_json?(value)) ? out.print("    #{value},\n") : out.print("    #{add_quotes(value)},\n")
         end
       end
-      out.print("]}")
-    } 
+
+      out.print("  ]\n}")
+    }
 
     puts "#{rows.count} rows written to #{json_file_name}"
   end
 
   table_vals = conn.exec("SELECT * FROM #{table}")
+
   stock_file_name = "files/#{config["stock_file_name"]}"
+
   File.open(stock_file_name, "a") { |out|
-    out.print("#{table_vals.values[0]}")
+    table_vals.each do |row|
+      out.print("{")
+      row.values.each_with_index do |value, index|
+        index == row.values.count - 1 ? out.print("#{value}},\n") : out.print("#{value}, ")
+      end
+    end
   }
-  
-  tgz = Zlib::GzipWriter.new(File.open("stock.tgz", "wb"))
-  Minitar.pack(stock_file_name, tgz)
-  File.delete(stock_file_name)
-  
+
+  tgz = Zlib::GzipWriter.new(File.open("stock.tgz", "wb")) # create the archive
+  Minitar.pack(stock_file_name, tgz)                       # copy the stock file in it
+  File.delete(stock_file_name)                             # and delete the file
+
   puts "#{table_vals.cmd_tuples()} rows stocked in tar archive"
 
+  conn.exec("BEGIN") # for testing
   conn.exec("DELETE FROM #{table}")
   puts "#{table_vals.cmd_tuples()} rows deleted from the table #{table}"
-
   conn.exec("ROLLBACK") # for testing
 end
 
